05df354c0ac4d9668bab35cc58ef1d9fa21080ba
Cem Ekici <cemekici@gmail.com>
2018-10-24 13:52:11 +0200

    Test

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/main.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'src'),
     filename: 'widget.js'
   },
   module: {
